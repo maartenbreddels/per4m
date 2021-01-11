00ae03986aa2640afa81a983795661fe8618b532
Maarten A. Breddels <maartenbreddels@gmail.com>
2021-01-11 12:34:21 +0100

    remove example1, due to https://github.com/gaogaotiantian/viztracer/issues/81

--- per4m/example2.py
+++ per4m/example2.py
@@ -1,6 +1,7 @@
-# same as example1, but without explicit viztracer calls
+# shows high gil load, but no wait
 import threading
 import time
+import numpy as np
 
 # if we don't run with gil_load, we just skip it
 import gil_load
@@ -11,25 +12,35 @@ try:
 except RuntimeError:
     use_gil_load = False
 
-def some_computation():
+
+N = 1024*1024*32
+M = 4
+x = np.arange(N, dtype='f8')
+
+
+def some_numpy_computation():
     total = 0
-    for i in range(1_000_000):
-        total += i
+    for i in range(M):
+        total += x.sum()
     return total
 
 
-thread1 = threading.Thread(target=some_computation)
-thread2 = threading.Thread(target=some_computation)
+thread1 = threading.Thread(target=some_numpy_computation)
+thread2 = threading.Thread(target=some_numpy_computation)
+
+
 def main(args=None):
     thread1.start()
     thread2.start()
-    time.sleep(0.2)
+    total = 0
+    for i in range(2_000_000):
+        total += i
     for thread in [thread1, thread2]:
         thread.join()
+
     if use_gil_load:
         gil_load.stop()
         stats = gil_load.get()
         print(gil_load.format(stats))
-
 if __name__ == "__main__":
     main()
